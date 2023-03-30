890c14f946652021beaaa003d3ee25c8a6212a8a
juan <juanyarcidiacono@gmail.com>
2023-03-30 15:44:12 -0300

    modificacion

--- Ejercicio clase 1/ejercicio_1_clase1.py
+++ Ejercicio clase 1/ejercicio_1_clase1.py
@@ -5,28 +5,7 @@ Existen al menos dos metodos ampliamente utilizados para medir la temperatura am
 
 Formula:
 ºF = ºC x 1.8 + 32
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Ejercicio 2:
-
-Una practica cotidiana en desarrollo de software es mirar programas de otros. En este ejercicio, van a tener acceso al codigo, pueden probarlo con distintos numeros y verificar el resultado. Lo que les pido es que cuenten en sus palabras, lo que hace el programa.
-
-
-
-num1 = int(input("primer numero: "))
-num2 = int(input("segundo numero: "))
-
-producto = num1 * num2
-
-if producto < 1000:
-    print(producto)
-elif (num1 == 50) and (num2 == 50):
-    print("50 y 50")
-else:
-    print(num1 + num2)
-    """
+"""
 
 #Ejercicio 1
 import math
