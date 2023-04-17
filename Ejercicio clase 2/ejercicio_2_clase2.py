63a8e3d45eccba08621bb708ad0d3c7fd9f5676c
juan <juanyarcidiacono@gmail.com>
2023-04-17 17:52:55 -0300

    Clase 2

--- Ejercicio clase 2/ejercicio_2_clase2.py
+++ Ejercicio clase 2/ejercicio_2_clase2.py
@@ -6,12 +6,12 @@ si el promedio es mayor o igual a siete mostrar un mensaje "Promocionado".
 def main():
     promedio(7,8,7)
 
-
 def promedio(n1,n2,n3):
     
     if((n1+n2+n3)/3 >= 7):
         print("Promocionado")
-
+    else:
+        print("No esta promocionado")
 
 if __name__ == "__main__":
     main()
