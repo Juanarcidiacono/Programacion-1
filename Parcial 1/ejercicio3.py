eb106a12bfb183e8227fe6548f39585306037c2c
juan <juanyarcidiacono@gmail.com>
2023-05-05 08:13:07 -0300

    metodo count_letters agregado

--- Parcial 1/ejercicio3.py
+++ Parcial 1/ejercicio3.py
@@ -6,12 +6,12 @@ def input_word():
     word = list(input("Ingrese una palabra >> "))
     return word
 
-'''
+
 def count_letters(word, number_of_letters=0):
     for letter_pos in range(len(word)):
         number_of_letters = + letter_pos
     return number_of_letters
-'''
+
 
 def print_word_and_number_of_letters(word=None, key=list("salir")):
     while word != key:
@@ -19,11 +19,9 @@ def print_word_and_number_of_letters(word=None, key=list("salir")):
         for letter in word:
             print(letter)
 
-        print(f"La cantidad de letras es: {len(word)}")
-
-    ''' 
-       print(f"La cantidad de letras es: {count_letters(word)}") #se pudo haber utilizado el metodo len directamente. Supuse que la idea del ejercicio era usar un ciclo for con contador. Se pudo haber utilizado len(word)
-    '''
+        #print(f"La cantidad de letras es: {len(word)}")
+        print(f"La cantidad de letras es: {count_letters(word)}") #se pudo haber utilizado el metodo len directamente. Supuse que la idea del ejercicio era usar un ciclo for con contador. Se pudo haber utilizado len(word)
+    
     
 
     print("Saliendo del programa")
