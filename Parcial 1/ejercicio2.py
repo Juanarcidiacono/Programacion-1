6e7c380cfdac5b460ee5c2ffb7b16a759afa3ec6
juan <juanyarcidiacono@gmail.com>
2023-05-07 20:42:14 -0300

    varios

--- Parcial 1/ejercicio2.py
+++ Parcial 1/ejercicio2.py
@@ -2,25 +2,27 @@ def main():
     n1_divisble_by_n2()
 
 def input_numbers():
-    n1 = input("Ingrese el numero >> ")
-    n2 = input("Es divisible por >> ")
-    
+    n1 = int(input("Ingrese el numero >> "))
+    n2 = int(input("Es divisible por >> "))
+
     return [n1,n2]
 
-def n1_divisble_by_n2(n2_is_not_zero=True):
+def n1_divisble_by_n2(n2_is_not_equat_to_zero=True):
 
-    while n2_is_not_zero:
+    while n2_is_not_equat_to_zero:
+
         numbers = input_numbers()
         n1 = int(numbers[0])
         n2 = int(numbers[1])
 
         if n2 != 0:
-            if (n1 % n2 == 0):
+            if n1 % n2 == 0:
                 print("Si")
             else:
                 print("No")
         else:
-            n2_is_not_zero = False
+            n2_is_not_equat_to_zero = False
+
     else:
         print("No es posible dividir por cero")
 
