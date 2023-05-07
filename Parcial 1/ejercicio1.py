75375b6659823d7e310cb52244e1c0f6e9e91ec0
juan <juanyarcidiacono@gmail.com>
2023-05-07 19:33:33 -0300

    varios

--- Parcial 1/ejercicio1.py
+++ Parcial 1/ejercicio1.py
@@ -1,9 +1,7 @@
 def main():
     sum_and_print_even_numbers()
 
-def input_number():
-    is_number_correct = False
-
+def input_number(is_number_correct=False):
     while not is_number_correct:
         try:
             number = int(input("Ingrese un numero >> "))
@@ -17,9 +15,9 @@ def input_number():
 def calculate_even_numbers_given_a_number():
     # Solicitar al usuario un valor de tipo entero y guardarlo en una variable.
     number = input_number()
-    # Calculo los numeros pares y los coloco en una lista
+    # Calculo los numeros pares y los coloco en una lista.
     even_numbers = [i for i in range(number + 1) if i % 2 == 0]
-    # Elimino el 0 por ser numero neutro
+    # Elimino el 0 por ser numero neutro.
     even_numbers.pop(0)
 
     return even_numbers
@@ -27,7 +25,8 @@ def calculate_even_numbers_given_a_number():
 def sum_and_print_even_numbers(sum=0):
     list_even_numbers = calculate_even_numbers_given_a_number()
 
-    # Imprimo cada uno de los numeros. Creo un contador que contiene la suma acumulado de cada uno de los elementos de la lista de numeros pares.
+    # Imprimo cada uno de los numeros. Creo un contador que contiene 
+    # la suma acumulada de cada uno de los elementos de la lista de numeros pares.
     for number in list_even_numbers:
         print(number)
         sum +=  number
--- Parcial 1/ejercicio3.py
+++ Parcial 1/ejercicio3.py
@@ -2,7 +2,8 @@ def main():
     print_word_and_number_of_letters()
 
 def input_word():
-    # El usuario ingresa una palabra. Lo convierto a una lista para que sea mas facil contar los caracteres
+    # El usuario ingresa una palabra. Lo convierto 
+    # a una lista para que sea mas facil contar los caracteres.
     word = list(input("Ingrese una palabra >> "))
      
     return word
@@ -12,32 +13,26 @@ def count_letters(word, number_of_letters=0):
         number_of_letters =+  letter_pos
 
     return number_of_letters
-'''
-def print_word_and_number_of_letters(word=None, key=list("salir")):
-    while word != key:
-        word = input_word()
-        for letter in word:
-            print(letter)
-        #print(f"La cantidad de letras es: {len(word)}")
-        print(f"La cantidad de letras es: {count_letters(word)}") #se pudo haber utilizado el metodo len directamente. Supuse que la idea del ejercicio era usar un ciclo for con contador. Se pudo haber utilizado len(word)
-    
-    print("Saliendo del programa")
-'''
-def print_word_and_number_of_letters(word_is_not_salir=True, key=list("salir")):
+
+def print_word_and_number_of_letters(word_is_not_salir=True, salir=list("salir")):
     while word_is_not_salir:
         word = input_word()
-        if word == key:
+        if word == salir:
             word_is_not_salir = False
         else:
             for letter in word:
                 print(letter)
-            #print(f"La cantidad de letras es: {len(word)}")
-            print(f"La cantidad de letras es: {count_letters(word)}") #se pudo haber utilizado el metodo len directamente. Supuse que la idea del ejercicio era usar un ciclo for con contador. Se pudo haber utilizado len(word)
+
+            print(f"La cantidad de letras es: {count_letters(word)}") 
+
+            '''
+            Se pudo haber utilizado el metodo len() directamente. 
+            Supuse que la idea del ejercicio era usar un ciclo for con contador. 
+            Se pudo haber utilizado len(word) en lugar de count_letters(word)
+            '''
     else:
         print("Saliendo del programa")
 
 
-
-            
 if __name__ == "__main__":
     main()
