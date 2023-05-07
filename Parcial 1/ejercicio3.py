9174e2ff823dd46dd82ea9f87c7162503605f959
juan <juanyarcidiacono@gmail.com>
2023-05-06 22:08:09 -0300

    cambios varios

--- Parcial 1/ejercicio3.py
+++ Parcial 1/ejercicio3.py
@@ -7,27 +7,37 @@ def input_word():
      
     return word
 
-
 def count_letters(word, number_of_letters=0):
     for letter_pos in range(len(word)+1):
         number_of_letters =+  letter_pos
 
     return number_of_letters
-
-
+'''
 def print_word_and_number_of_letters(word=None, key=list("salir")):
     while word != key:
         word = input_word()
         for letter in word:
             print(letter)
-
         #print(f"La cantidad de letras es: {len(word)}")
         print(f"La cantidad de letras es: {count_letters(word)}") #se pudo haber utilizado el metodo len directamente. Supuse que la idea del ejercicio era usar un ciclo for con contador. Se pudo haber utilizado len(word)
     
-    
-
     print("Saliendo del programa")
+'''
+def print_word_and_number_of_letters(word_is_not_salir=True, key=list("salir")):
+    while word_is_not_salir:
+        word = input_word()
+        if word == key:
+            word_is_not_salir = False
+        else:
+            for letter in word:
+                print(letter)
+            #print(f"La cantidad de letras es: {len(word)}")
+            print(f"La cantidad de letras es: {count_letters(word)}") #se pudo haber utilizado el metodo len directamente. Supuse que la idea del ejercicio era usar un ciclo for con contador. Se pudo haber utilizado len(word)
+    else:
+        print("Saliendo del programa")
 
 
+
+            
 if __name__ == "__main__":
     main()
